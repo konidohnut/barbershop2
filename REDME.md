5d4c8d23b322f3fc81435189de50e1b9261e2b88
konidohnut <valer.novogran@gmail.com>
2022-04-17 13:04:51 +0200

    first commit

---
+++ REDME.md
@@ -0,0 +1,3 @@
+# My GitHub Project
+
+This is my Barbershop project. I published it directly from Visual Studio Code.
---
+++ index.js
@@ -0,0 +1,51 @@
+// 'use strict';
+// const details = {
+//     message: 'Hello!',
+// }
+// function getMessage(){
+//     return this.message;
+// }
+// console.log(getMessage.call(details));
+
+// const name = 'John';
+
+// function printName(){
+//     const name = 'Peter';
+//     console.log(name);
+// }
+// printName();
+
+// function foo(a, b){
+//     return a * b;
+// }
+
+// const bar = foo.bind(null, 2)
+
+// console.log(bar(2))
+
+// console.log(message);
+// var message = 'Hello';
+
+// "use strict";
+
+// function getThis(){
+//     return this;
+// }
+// console.log(getThis());
+
+// console.log(message);
+// let message = 'Hello';
+
+// printMessage();
+
+// function printMessage(){
+//     console.log('Hello!');
+// }
+
+// let f = function g() {return 23; };
+// console.log(typeof g());
+
+for(var i = 0; i < 10; i++){
+    //
+}
+console.log(i)
